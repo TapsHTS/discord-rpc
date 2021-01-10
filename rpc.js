3f06540a0aaadf0a5b177123ed29e2fbd24e8d8b
Janastinou <72787716+Janastinou@users.noreply.github.com>
2021-01-10 14:13:29 +0100

    Update rpc.js
    Au lieux de mettre l'ID manuellement, Le RPC va utiliser celui fournis par le Token.

--- rpc.js
+++ rpc.js
@@ -1,38 +1,39 @@
 const rpc = require('discord-rpc');
 const Discord = require('discord.js');
-const id = 'XXXXXXX'; // L'id de votre bot
 const token = 'XXXXXXXX'; // Le token de votre bot
 
-
-rpc.register(id)
 const bot = new Discord.Client()
 const client = new rpc.Client({transport: 'ipc'});
-client.on('ready', () => {
-    bot.on('ready', () =>{
-    console.log('RPC Démarée');
-    client.request('SET_ACTIVITY', {
-        pid: 9999,
-        activity: {
-            details: 'Best Music Bot', // Pour faire une description
-            assets: {
-                large_image: "large-key-name", // Clé de la grande image
-                large_text: "Add me !", // Text quand on passe sa souris sur l'image
-                small_image: "small-key-name", // Clé de la petite image
-                small_text: `💬 ${bot.guilds.cache.size} servers`, // Text quand on passe sa souris sur l'image
-            },
-            buttons: [
-                {
-                    label: "🎺 Invite", //Nom du bouton
-                    url: "https://top.gg/bot/660891282465357834" //lien du bouton
-                },
-                {
-                    label: "🚒 Support", //Nom du bouton
-                    url: "https://discord.io/neoport" //Lien du bouton
-                }
-            ]
-        }
-    })
-})
+
+
+bot.on('ready', () => {
+	client.login({clientId: bot.user.id});
+	rpc.register(bot.user.id)
+	client.on('ready', () => {
+		console.log('RPC Démarée');
+		client.request('SET_ACTIVITY', {
+			pid: 9999,
+			activity: {
+				details: 'Best Music Bot', // Pour faire une description
+				assets: {
+					large_image: "large-key-name", // Clé de la grande image
+					large_text: "Add me !", // Text quand on passe sa souris sur l'image
+					small_image: "small-key-name", // Clé de la petite image
+					small_text: `💬 ${bot.guilds.cache.size} servers`, // Text quand on passe sa souris sur l'image
+				},
+				buttons: [
+					{
+						label: "🎺 Invite", //Nom du bouton
+						url: "https://top.gg/bot/660891282465357834" //lien du bouton
+					},
+					{
+						label: "🚒 Support", //Nom du bouton
+						url: "https://discord.io/neoport" //Lien du bouton
+					}
+				]
+			}
+		});
+	});
 });
-client.login({clientId: id});
-bot.login(token)
+
+bot.login(token);
